de5a3274d4c81ad63035c133d96041b89cf6363e
Gavin Doyle <doyle.d.gavin@gmail.com>
2024-10-03 14:40:36 +0100

    Fixed testimonial width on mobile screen and added lazy loading

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -60,7 +60,7 @@ function Testimonials() {
         {testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
-            className="relative group mx-auto"
+            className="relative group mx-auto w-9/10"
             onMouseEnter={() => setHovered(testimonial.id)}
             onMouseLeave={() => setHovered(null)}
             onClick={() => navigate(testimonial.galleryLink)}
@@ -70,7 +70,8 @@ function Testimonials() {
               src={testimonial.image}
               alt="Testimonial"
               className="w-full max-w-md h-64 object-cover opacity-70 rounded-3xl rounded-tr-none cut-corner::after transform transition-transform  group-hover:scale-105"
-              style={{ clipPath: 'url(#benefits)' }} />
+              style={{ clipPath: 'url(#benefits)' }} 
+              loading = 'lazy'/>
             <div className="absolute max-w-md inset-0 flex items-center justify-center text-white text-xl font-semibold bg-black bg-opacity-50 opacity-0 group-hover:opacity-100  group-hover:scale-105 transition-opacity" style={{ clipPath: 'url(#benefits)' }}>
               {testimonial.text}
             </div>
